5a671b7f2f22c151b791ae6af34e4687db1ad94c
fangarh <dvornikov.d@gmail.com>
2024-02-27 19:57:15 +0300

    ///

--- HomeTask/BusinessLogic/GameLogic/Game/GuessNumberGame.cs
+++ HomeTask/BusinessLogic/GameLogic/Game/GuessNumberGame.cs
@@ -2,6 +2,7 @@
 using BusinessContracts.Helpers;
 using BusinessContracts.Logger;
 using GameLogic.Helpers;
+using GameLogic.Logger;
 
 namespace GameLogic.Game
 {
@@ -11,16 +12,44 @@ namespace GameLogic.Game
         private readonly IRandomNumberGenerator _randomizer;
         private readonly IMyLogger _logger;
 
+        public int WinCount { get; }
+        public int LooseCount { get; }
+
         public GuessNumberGame(IGuessChecker checker = null, IRandomNumberGenerator randomizer = null, IMyLogger logger = null)
         {
             _checker = checker ?? new GuessChecker();
-            _randomizer = randomizer;
-            _logger = logger;
+            _randomizer = randomizer ?? new ExtRandomNumberGenerator(500);
+            _logger = logger ?? new MyMemoryLogger();
         }
 
         public void Play(int tryCount, int roundCount = 1)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < roundCount; i++)
+            {
+                int number = _randomizer.RandomNumber;
+                Console.WriteLine($"Раунд №{i} начался. Для выхода введите '\\q' или '\\в'");
+
+                switch (PlayRound(tryCount))
+                {
+                    
+                }
+            }
+        }
+
+        private RoundResult PlayRound(int tryCount)
+        {
+            for (int j = 0; j < tryCount; j++)
+            {
+                Console.WriteLine($"Введите число: ");
+                var inStr = Console.ReadLine();
+
+                if (!Int32.TryParse(inStr, out var choice))
+                {
+
+                }
+            }
+
+            return RoundResult.Loose;
         }
     }
 }
